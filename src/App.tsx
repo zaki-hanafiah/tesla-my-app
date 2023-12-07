8b848a5c0fb7a61e65271acb024a5f48df5fec26
zaki-hanafiah <zaki@zakhov.com>
2023-12-07 21:22:00 +0800

    feat: add persistent storage hook via idb keyval lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,66 @@
 import React from "react";
 import logo from "./logo.svg";
-import { set, get } from "idb-keyval";
+import { usePersistedState } from "./hooks/usePersistedState";
 import "./App.css";
 import * as serviceWorker from "./serviceWorkerRegistration";
 
 function App() {
-  const [keyVal, setKeyVal] = React.useState<string>("");
+  const [darkModeOn, setDarkModeOn] = usePersistedState<boolean>(
+    "darkModeOn",
+    true,
+  );
 
   serviceWorker.register();
 
-  async function testIDBKeyval() {
-    await set("hello", "world");
-    const whatDoWeHave = await get("hello");
-    setKeyVal(whatDoWeHave);
-  }
+  const handleOnChange = ({ target }: React.ChangeEvent<HTMLInputElement>) =>
+    setDarkModeOn(target.checked);
 
-  testIDBKeyval();
+  const styles = {
+    ...(darkModeOn
+      ? {
+          backgroundColor: "black",
+          color: "white",
+        }
+      : {
+          backgroundColor: "white",
+          color: "black",
+        }),
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello {keyVal}</p>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {darkModeOn === undefined ? (
+        <>Loading preferences...</>
+      ) : (
+        <>
+          <header className="App-header" style={styles}>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <p>
+              <input
+                type="checkbox"
+                value="darkMode"
+                checked={darkModeOn}
+                id="darkModeOn"
+                name="darkModeOn"
+                style={{ width: "3rem", height: "3rem" }}
+                onChange={handleOnChange}
+              />
+              <label htmlFor="darkModeOn">Use dark mode?</label>
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </>
+      )}
     </div>
   );
 }
