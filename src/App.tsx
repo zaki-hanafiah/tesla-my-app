7b4cfd45da1c6c7e431eaf4121ea22c4fb32f163
zaki-hanafiah <zaki@zakhov.com>
2023-12-07 16:35:56 +0800

    chore: add idb-keyval lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
+import { set, get } from 'idb-keyval';
 import './App.css';
+import * as serviceWorker from './serviceWorkerRegistration';
 
 function App() {
+  const [keyVal, setKeyVal] = React.useState<string>('');
+
+  serviceWorker.register();
+
+async function testIDBKeyval() {
+    await set('hello', 'world');
+    const whatDoWeHave = await get('hello');
+    setKeyVal(whatDoWeHave);
+}
+
+testIDBKeyval();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello {keyVal}</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
