c912d5ea69ff344c3c35002e6fca46223524f1f1
zaki-hanafiah <zaki@zakhov.com>
2023-12-22 15:49:43 +0800

    chore: rollback styled-components to v5

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { Grommet, Heading, Main, CheckBox } from "grommet";
+import { Grommet, Heading, Main, CheckBox, Paragraph } from "grommet";
 import { usePersistedState } from "./hooks/usePersistedState";
 import * as serviceWorker from "./serviceWorkerRegistration";
-import { Paragraph } from "grommet/es6";
 
 function App() {
   const [darkModeOn, setDarkModeOn] = usePersistedState<boolean>(
@@ -15,48 +14,26 @@ function App() {
   const handleOnChange = ({ target }: React.ChangeEvent<HTMLInputElement>) =>
     setDarkModeOn(target.checked);
 
-  const styles = {
-    ...(darkModeOn
-      ? {
-          backgroundColor: "black",
-          color: "white",
-        }
-      : {
-          backgroundColor: "white",
-          color: "black",
-        }),
-  };
-
   return (
-    <Grommet themeMode={darkModeOn ? "dark" : "light"} full={true}>
+    <Grommet themeMode={darkModeOn ? "dark" : "light"} full>
       {darkModeOn === undefined ? (
         <>Loading preferences...</>
       ) : (
-        <Main background={darkModeOn ? "black" : "white"} pad="large">
-          <Heading className="App-header" style={styles}>
+        <Main background={darkModeOn ? "black" : "white"} pad="large" direction="column">
+          <Heading className="App-header">
             Hello World
           </Heading>
           <Paragraph>
             Edit <code>src/App.tsx</code> and save to reload.
           </Paragraph>
-          <Paragraph>
             <CheckBox
-              value="darkMode"
-              checked={darkModeOn}
-              label="Use dark mode?"
-              id="darkModeOn"
-              name="darkModeOn"
-              onChange={handleOnChange}
+                value="darkMode"
+                checked={darkModeOn}
+                label="Use dark mode?"
+                id="darkModeOn"
+                name="darkModeOn"
+                onChange={handleOnChange}
             />
-          </Paragraph>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </Main>
       )}
     </Grommet>
