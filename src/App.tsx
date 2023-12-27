da741dafbcbd59fd2d673e88ae5a84e10a0c5fd6
zaki-hanafiah <zaki@zakhov.com>
2023-12-27 23:12:08 +0800

    chore: use StoreProvider in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Grommet, Heading, Main, CheckBox, Paragraph } from "grommet";
 import { usePersistedState } from "hooks";
 import * as serviceWorker from "./serviceWorkerRegistration";
+import { StoreProvider } from "store";
 
 function App() {
   const [darkModeOn, setDarkModeOn] = usePersistedState<boolean>(
@@ -15,30 +16,32 @@ function App() {
     setDarkModeOn(target.checked);
 
   return (
-    <Grommet themeMode={darkModeOn ? "dark" : "light"} full>
-      {darkModeOn === undefined ? (
-        <>Loading preferences...</>
-      ) : (
-        <Main
-          background={darkModeOn ? "black" : "white"}
-          pad="large"
-          direction="column"
-        >
-          <Heading className="App-header">Hello World</Heading>
-          <Paragraph>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </Paragraph>
-          <CheckBox
-            value="darkMode"
-            checked={darkModeOn}
-            label="Use dark mode?"
-            id="darkModeOn"
-            name="darkModeOn"
-            onChange={handleOnChange}
-          />
-        </Main>
-      )}
-    </Grommet>
+    <StoreProvider>
+      <Grommet themeMode={darkModeOn ? "dark" : "light"} full>
+        {darkModeOn === undefined ? (
+          <>Loading preferences...</>
+        ) : (
+          <Main
+            background={darkModeOn ? "black" : "white"}
+            pad="large"
+            direction="column"
+          >
+            <Heading className="App-header">Hello World</Heading>
+            <Paragraph>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </Paragraph>
+            <CheckBox
+              value="darkMode"
+              checked={darkModeOn}
+              label="Use dark mode?"
+              id="darkModeOn"
+              name="darkModeOn"
+              onChange={handleOnChange}
+            />
+          </Main>
+        )}
+      </Grommet>
+    </StoreProvider>
   );
 }
 
